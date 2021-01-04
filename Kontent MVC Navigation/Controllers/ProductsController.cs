f176b1c89ca6358d03a02a639f4bd9b8981986fa
MichaelB <MichaelB@kentico.com>
2021-01-04 10:43:47 -0500

    Updated ProductsController route attribute

--- Kontent MVC Navigation/Controllers/ProductsController.cs
+++ Kontent MVC Navigation/Controllers/ProductsController.cs
@@ -8,7 +8,7 @@ namespace Kontent_MVC_Navigation.Controllers
 {
     public class ProductsController : Controller
     {
-        [Route("product-catalog", Name = "product-catalog")]
+        [Route("products", Name = "products")]
         public IActionResult Index()
         {
             return View();
